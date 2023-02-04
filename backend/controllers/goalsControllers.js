f377fc98e4494ad896e5db43396c86f0a8ad7c98
Rakesh <rakesh.v6@cognizant.com>
2023-02-04 09:12:30 +0530

    cloud mongodb added to node

--- backend/controllers/goalsControllers.js
+++ backend/controllers/goalsControllers.js
@@ -1,28 +1,53 @@
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler');
+const Goal = require('../model/goalModel');
 
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'get from controller' });
-})
+  const goals = await Goal.find();
+  res.status(200).json(goals);
+});
 
 const setGoals = asyncHandler(async (req, res) => {
-    if (!req.body.text) {
-        res.status(400);
-        throw new Error('Please enter text field');
-    };
-    res.status(200).json({ message: 'set from controller' });
-})
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error('Pls enter text field');
+  }
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+  res.status(200).json(goal);
+});
 
 const putGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `put Goals controller ${req.params.id}` })
-})
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error('Goal not found');
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
+});
 
 const deleteGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `delete Goals controller ${req.params.id}` })
-})
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error('Goal not found');
+  }
+
+  await goal.remove();
+
+  res.status(200).json({ id: req.params.id });
+});
 
 module.exports = {
-    getGoals,
-    setGoals,
-    putGoals,
-    deleteGoals
+  getGoals,
+  setGoals,
+  deleteGoals,
+  putGoals,
 };
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const { errorHandler } = require('../backend/middleware/errorMiddleware')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+const { errorHandler } = require('../backend/middleware/errorMiddleware');
 const port = process.env.PORT || 5001;
+const connectDB = require('./config/db');
 
+connectDB();
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/goals', require('./routes/goalRoutes'))
-app.use(errorHandler)
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
-})
+  console.log(`server started on port ${port}`);
+});
